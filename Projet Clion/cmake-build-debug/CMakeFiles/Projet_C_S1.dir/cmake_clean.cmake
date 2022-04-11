433cab64ec16f58979d1fa0209be526c891434ac
Rami <habache.rami@gmail.com>
2022-04-11 14:59:45 +0200

    clion

---
+++ Projet Clion/cmake-build-debug/CMakeFiles/Projet_C_S1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projet_C_S1.dir/main.c.obj"
+  "CMakeFiles/Projet_C_S1.dir/main.c.obj.d"
+  "Projet_C_S1.exe"
+  "Projet_C_S1.exe.manifest"
+  "Projet_C_S1.pdb"
+  "libProjet_C_S1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projet_C_S1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
